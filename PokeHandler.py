06d63f63ee361a10a032bedf3ea8becc4adc0724
Malte Kießling <mkalte666@googlemail.com>
2015-01-27 11:17:15 +0100

    Added Script for !spell command.
    Apllies mostly the the #exploders-channel on mibbit

--- PokeHandler.py
+++ PokeHandler.py
@@ -9,5 +9,5 @@ def HandlerFunc(client, event, prefix, command, params, trailing):
 	if params != client.nick:
 		if command == "PRIVMSG":
 			if trailing.find(client.nick) != -1:
-				if trailing.find("poke") != -1:
+				if trailing.find("poke") != -1 or trailing.find("smack") != -1 or trailing.find("hits") != -1:
 					client.SendMessage(params, random.choice(worddb.generalInsults))
---
+++ SpellHandler.py
@@ -0,0 +1,21 @@
+import random
+import re
+import IrcClient
+import worddb
+import imp
+import sys
+
+def HandlerFunc(client, event, prefix, command, params, trailing):
+	if params != client.nick:
+		if command == "PRIVMSG":
+			e = re.compile('!spell\s+('+client.nick+')\s*\w*')
+			match = re.search(e,trailing)
+			if match!=None:
+				e = re.compile(ur'([\w]*)![\w@.-]*')
+				match = re.search(e,prefix)
+				if match!=None:
+					if client.IsIgnored(match.group(1))!=True:
+						if random.randint(0,2)!=0:
+							client.SendMessage(params, "!spell "+match.group(1))
+						else:
+							client.SendMessage(params, random.choice(worddb.generalInsults))
